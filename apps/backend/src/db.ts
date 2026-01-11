4fca604d42a22eddd7f8fa7817e6b3ac31e53c5d
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2026-01-11 17:14:14 -0500

    refactor(backend): reorganize lib structure and move core files

--- apps/backend/src/db.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-
-import { PrismaClient } from "../prisma/client/client";
-import { DATABASE_URL } from "./config/env";
-
-const adapter = new PrismaPg({
-  connectionString: DATABASE_URL,
-});
-export const db = new PrismaClient({ adapter });
