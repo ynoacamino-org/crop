cf81e63a744d26bad0670c393cdb99a659f28c5e
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2026-01-13 16:04:05 -0500

    refactor(config): rename BASE_URL to API_BASE_URL and add INTERNAL_API_URL

--- apps/frontend/src/shared/config/env.ts
+++ apps/frontend/src/shared/config/env.ts
@@ -1,2 +1,4 @@
-export const { NEXT_PUBLIC_BASE_URL: BASE_URL = "http://localhost:8000" } =
-  process.env;
+export const {
+  NEXT_PUBLIC_API_URL: API_URL = "http://localhost:8000/api",
+  INTERNAL_API_URL = "http://backend:8000/api",
+} = process.env;
