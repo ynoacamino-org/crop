4a198977ef6aee6e90026d8ccbc65fef4226171a
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2026-01-13 16:06:34 -0500

    refactor(frontend): remove old gql directory and add got dependency

--- apps/frontend/src/gql/service.client.ts
+++
@@ -1,8 +0,0 @@
-import { cacheExchange, fetchExchange, Client as Service } from "urql";
-import { BASE_URL } from "@/shared/config/env";
-import { dateExchange } from "./exchanges/date";
-
-export const service = new Service({
-  url: `${BASE_URL}/api/graphql`,
-  exchanges: [cacheExchange, dateExchange, fetchExchange],
-});
